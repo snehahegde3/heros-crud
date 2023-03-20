ff0af45332fb5bca850764c2b87ec360376b8fec
Sneha Hegde <snehahegde33@gmail.com>
2023-03-20 23:29:27 +0530

    db connection made

---
+++ SupaHerosAPI/SupaHerosAPI/Data/DataContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace SupaHerosAPI.Data
+{
+    public class DataContext: DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options): base(options){}
+
+        public DbSet<SupaHero> SupaHeros => Set<SupaHero>();
+    }
+}
--- SupaHerosAPI/SupaHerosAPI/Program.cs
+++ SupaHerosAPI/SupaHerosAPI/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.EntityFrameworkCore;
+using SupaHerosAPI.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,11 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<DataContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConfiguration"));
+});
+
 builder.Services.AddCors(options => options.AddPolicy(name: "SupaHeroOrigins", 
     policy =>
     {
